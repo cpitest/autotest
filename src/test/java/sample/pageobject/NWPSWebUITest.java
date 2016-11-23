81408ee1c9646b107eab8d02ffb1de6211810d4f
sbcnakamura <nakamura.hajime@sharp.co.jp>
2016-11-23 17:16:24 +0900

    test

--- src/test/java/sample/pageobject/NWPSWebUITest.java
+++ src/test/java/sample/pageobject/NWPSWebUITest.java
@@ -28,12 +28,12 @@ public class NWPSWebUITest{
 		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 		capabilities.setCapability("marionette", true);
 		driver = new FirefoxDriver(capabilities);
-		//
-		// baseUrl = "https://cvs.so.sh.airfolc.co.jp/";
+
+		baseUrl = "https://cvs.so.sh.airfolc.co.jp/";
 		// driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
 		// ログイン
-		// driver.get(baseUrl + "/sharp_netprint/ja/top.aspx");
+		driver.get(baseUrl + "/sharp_netprint/ja/top.aspx");
 		// driver.findElement(By.id("txtId")).clear();
 		// driver.findElement(By.id("txtId")).sendKeys("nakamura.hajime@sharp.co.jp");
 		// driver.findElement(By.id("txtPw")).clear();
