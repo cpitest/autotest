1b6d361dd0f0b7a08b74c9b417b6edd9caa72bdf
sbc-tsujii <tsujii.yoshihiko@sharp.co.jp>
2017-01-11 10:19:19 +0900

    ご利用上の注意点を追加

---
+++ src/test/java/sample/pageobject/TopsFooterNoticeOnUseTest.java
@@ -0,0 +1,61 @@
+package sample.pageobject;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import sample.pageobject.util.TestUtil;
+import selenium.WebDriverWrapper;
+
+/**
+ * トップページのフッターのご利用上の注意点のクリックに関するテスト
+ *
+ */
+public class TopsFooterNoticeOnUseTest{
+	private WebDriver driver;
+	private String baseUrl;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		driver = new WebDriverWrapper("chrome");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		baseUrl = "https://cvs.so.sh.airfolc.co.jp/";
+	}
+
+	@Test
+	public void ご利用上の注意を開く() throws Exception{
+		driver.get(baseUrl + "sharp_netprint/ja/top.aspx");
+
+		// 現在のウインドウIDを取得
+		String current_window_id = driver.getWindowHandle();
+
+		driver.findElement(By.linkText("ご利用上の注意")).click();
+
+		// 新しく開かれたウィンドウにフォーカスする
+		TestUtil.focusByNewWindow(driver, current_window_id);
+		assertEquals("https://cvs.so.sh.airfolc.co.jp/sharp_netprint/ja/notice.aspx", driver.getCurrentUrl());
+		TestUtil.takesScreenshot(driver, "goriyouzyou1.png");
+		driver.close();
+		driver.switchTo().window(current_window_id);
+
+	}
+
+	@After
+	public void tearDown() throws Exception{
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if(!"".equals(verificationErrorString)){
+			fail(verificationErrorString);
+		}
+	}
+
+}
